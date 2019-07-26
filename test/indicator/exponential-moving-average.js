f91a6ada979b05a165c3de3ddce36a243907f0ce
codeplayr <nufan2k@gmx.de>
2019-07-26 12:05:14 +0200

    add EMA testcase

--- test/indicator/exponential-moving-average.js
+++ test/indicator/exponential-moving-average.js
@@ -100,6 +100,47 @@ describe('EMA', function(){
 		]
 	};
 
+	var arr_3 = {
+		items: [
+			22.81,
+			23.09,
+			22.91,
+			23.23,
+			22.83,
+			23.05,
+			23.02,
+			23.29,
+			23.41,
+			23.49,
+			24.60,
+			24.63,
+			24.51,
+			23.73,
+			23.31,
+			23.53,
+			23.06,
+			23.25,
+			23.12,
+			22.80,
+			22.84,
+		],
+		range: 9,
+		results: [
+			23.07,
+			23.18,
+			23.47,
+			23.70,
+			23.86,
+			23.83,
+			23.73,
+			23.69,
+			23.56,
+			23.50,
+			23.42,
+			23.30,
+			23.21,
+		]
+	};
 
 	it('should calculate correctly and return results', function(){
 		var ema = new EMA();
@@ -123,6 +164,21 @@ describe('EMA', function(){
 		assert.isTrue( arr_2.items.length == arr_2.results.length );
 		results.forEach( (item, idx) => {
 			assert.closeTo( item.ema, arr_2.results[ idx ], 0.02 );
+		});
+	});
+
+	it('should calculate correctly with range and return results', function(){
+		var ema = new EMA( {range: arr_3.range } );
+		arr_3.items.forEach( ( item ) => {
+			ema.add( item );
+		});
+
+		var results = ema.calculate();
+		assert.isArray( results );
+		assert.isTrue( arr_3.items.length - (arr_3.range - 1) == arr_3.results.length );
+
+		results.forEach( (item, idx) => {
+			assert.closeTo( item.ema, arr_3.results[ idx ], 0.05 );
 		});
 	});
 
