dfba76cfd6e20b34fff3258ce6d486108ea3255c
codeplayr <nufan2k@gmx.de>
2019-08-30 02:06:13 +0200

    optimize

--- test/indicator/relative-strength-index.js
+++ test/indicator/relative-strength-index.js
@@ -31,24 +31,23 @@ describe('RSI', function(){
 		45.71,
 	];
 
-	it('should calculate correctly and return results', () => {
-		let rsi = new RSI();
+	let runTest = function( data, options, expectedResultLength ) {
+		let rsi = new RSI( options );
 		rsi.setValues( data );
 
 		let result = rsi.calculate();
 
-		assert.isFalse( result.error );
-		assert.isArray( result.collection );
-		assert.isTrue( result.collection.length ==  data.length );
+		assert.isArray( result );
+		assert.isTrue( result.length ==  expectedResultLength  );
 
-		let item_14 = result.collection[ 13 ];
+		let item_14 = result[ 13 ];
 
 		assert.isNull( item_14.avg_gain );
 		assert.isNull( item_14.avg_loss );
 		assert.isNull( item_14.rs );
 		assert.isNull( item_14.rsi );
 
-		let last_item  = result.collection[result.collection.length - 1 ];
+		let last_item  = result[result.length - 1 ];
 
 		assert.isTrue( last_item.price == 45.71 );
 		assert.isTrue( last_item.gain == 0 );
@@ -56,16 +55,27 @@ describe('RSI', function(){
 		assert.closeTo( last_item.loss, 0.5, 0.05 );
 		assert.closeTo( last_item.rs, 1.3, 0.1 );
 		assert.closeTo( last_item.rsi, 58, 1 );
+	}
+
+	it('should calculate correctly and return results', () => {
+		runTest( data, {periods: 14}, data.length );
+	});
+
+	it('should calculate correctly with ranges and return results', () => {
+		let dataCopy = [...data];
+		dataCopy.unshift(40.11);
+		dataCopy.push(45.99);
+		runTest( dataCopy,  {periods: 14, startIndex: 1, endIndex: dataCopy.length - 2}, dataCopy.length - 2 );  
 	});
 
 	it('should throw error on invalid options', () => {
 		// timeperiod above array length
-		let rsi = new RSI( {focusIndex: data.length - 1, timePeriods: data.length } );
+		let rsi = new RSI( {endIndex: data.length - 1, periods: data.length } );
 		rsi.setValues( data );
 		assert.throw( () => rsi.calculate(), Error );
 
 		// focusIndex above array length
-		rsi = new RSI( {focusIndex: data.length, timePeriods: 14 } );
+		rsi = new RSI( {endIndex: data.length, periods: 14 } );
 		rsi.setValues( data );
 		assert.throw( () => rsi.calculate(), Error );
 
