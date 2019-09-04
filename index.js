a493cca5ccc3ba13bb2fedbbb652836b25f7264f
codeplayr <nufan2k@gmx.de>
2019-09-05 01:45:44 +0200

    update

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 'use strict';
 
-const ATR = require('./lib/indicator/average-true-range');
-const BB = require('./lib/indicator/bollinger-bands');
-const EMA = require('./lib/indicator/exponential-moving-average');
-const LWMA = require('./lib/indicator/linearly-weighted-moving-average');
-const MACD = require('./lib/indicator/moving-average-convergence-divergence');
-const OBV = require('./lib/indicator/on-balance-volume');
-const RSI = require('./lib/indicator/relative-strength-index');
-const SMA = require('./lib/indicator/simple-moving-average');
-const SO = require('./lib/indicator/stochastic-oscillator');
+const atr = require('./lib/indicator/average-true-range');
+const bb = require('./lib/indicator/bollinger-bands');
+const ema = require('./lib/indicator/exponential-moving-average');
+const lwma = require('./lib/indicator/linearly-weighted-moving-average');
+const macd = require('./lib/indicator/moving-average-convergence-divergence');
+const obv = require('./lib/indicator/on-balance-volume');
+const rsi = require('./lib/indicator/relative-strength-index');
+const sma = require('./lib/indicator/simple-moving-average');
+const so = require('./lib/indicator/stochastic-oscillator');
 
 module.exports = {
-	averageTrueRange: ATR,
-	ATR,
-	bollingerBands: BB,
-	BB,
-	exponentialMovingAverage: EMA,
-	EMA,
-	linearlyWeightedMovingAverage: LWMA,
-	LWMA,
-	movingAverageConvergenceDivergence: MACD,
-	MACD,
-	onBalanceVolume: OBV,
-	OBV,
-	relativeStrengthIndex: RSI,
-	RSI,
-	simpleMovingAverage: SMA,
-	SMA,
-	stochasticOscillator: SO,
-	SO,
+	averageTrueRange: atr,
+	atr,
+	bollingerBands: bb,
+	bb,
+	exponentialMovingAverage: ema,
+	ema,
+	linearlyWeightedMovingAverage: lwma,
+	lwma,
+	movingAverageConvergenceDivergence: macd,
+	macd,
+	onBalanceVolume: obv,
+	obv,
+	relativeStrengthIndex: rsi,
+	rsi,
+	simpleMovingAverage: sma,
+	sma,
+	stochasticOscillator: so,
+	so,
 }
--- tests.js
+++ tests.js
@@ -1,2 +1,8 @@
-require('./test/indicator/relative-strength-index');
+require('./test/utils/math');
+require('./test/utils/number');
+require('./test/indicator/simple-moving-average');
 require('./test/indicator/exponential-moving-average');
+require('./test/indicator/linearly-weighted-moving-average');
+require('./test/indicator/relative-strength-index');
+
+
