90af96ef34af415d21198358d09487b021e01538
codeplayr <nufan2k@gmx.de>
2019-09-13 12:05:44 +0200

    fix

--- tests.js
+++ tests.js
@@ -2,6 +2,7 @@ require('./test/utils/math');
 require('./test/utils/number');
 require('./test/indicator/simple-moving-average');
 require('./test/indicator/exponential-moving-average');
-require('./test/indicator/linearly-weighted-moving-average');
 require('./test/indicator/relative-strength-index');
+require('./test/indicator/bollinger-bands');
 require('./test/indicator/average-true-range');
+
