bdd1773150b2ff7f3234157f04bad7cfe9ff3ead
codeplayr <nufan2k@gmx.de>
2019-10-02 14:36:51 +0200

    add tests

--- tests.js
+++ tests.js
@@ -2,7 +2,10 @@ require('./test/utils/math');
 require('./test/utils/number');
 require('./test/indicator/simple-moving-average');
 require('./test/indicator/exponential-moving-average');
+require('./test/indicator/linearly-weighted-moving-average');
 require('./test/indicator/relative-strength-index');
 require('./test/indicator/bollinger-bands');
 require('./test/indicator/average-true-range');
-
+require('./test/indicator/moving-average-convergence-divergence');
+require('./test/indicator/on-balance-volume');
+require('./test/indicator/stochastic-oscillator');
