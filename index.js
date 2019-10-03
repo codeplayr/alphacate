24d026e2fadedf3df030ec85f81201d7a5a8b25a
codeplayr <nufan2k@gmx.de>
2019-10-04 01:22:48 +0200

    change to uppercase

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 'use strict';
 
-const atr = require('./lib/indicator/average-true-range');
-const bb = require('./lib/indicator/bollinger-bands');
-const ema = require('./lib/indicator/exponential-moving-average');
-const lwma = require('./lib/indicator/linearly-weighted-moving-average');
-const macd = require('./lib/indicator/moving-average-convergence-divergence');
-const obv = require('./lib/indicator/on-balance-volume');
-const rsi = require('./lib/indicator/relative-strength-index');
-const sma = require('./lib/indicator/simple-moving-average');
-const so = require('./lib/indicator/stochastic-oscillator');
+const ATR = require('./lib/indicator/average-true-range');
+const BB = require('./lib/indicator/bollinger-bands');
+const EMA = require('./lib/indicator/exponential-moving-average');
+const LWMA = require('./lib/indicator/linearly-weighted-moving-average');
+const MACD = require('./lib/indicator/moving-average-convergence-divergence');
+const OBV = require('./lib/indicator/on-balance-volume');
+const RSI = require('./lib/indicator/relative-strength-index');
+const SMA = require('./lib/indicator/simple-moving-average');
+const SO = require('./lib/indicator/stochastic-oscillator');
 
 module.exports = {
-	averageTrueRange: atr,
-	atr,
-	bollingerBands: bb,
-	bb,
-	exponentialMovingAverage: ema,
-	ema,
-	linearlyWeightedMovingAverage: lwma,
-	lwma,
-	movingAverageConvergenceDivergence: macd,
-	macd,
-	onBalanceVolume: obv,
-	obv,
-	relativeStrengthIndex: rsi,
-	rsi,
-	simpleMovingAverage: sma,
-	sma,
-	stochasticOscillator: so,
-	so,
+	AverageTrueRange: ATR,
+	ATR,
+	BollingerBands: BB,
+	BB,
+	ExponentialMovingAverage: EMA,
+	EMA,
+	LinearlyWeightedMovingAverage: LWMA,
+	LWMA,
+	MovingAverageConvergenceDivergence: MACD,
+	MACD,
+	OnBalanceVolume: OBV,
+	OBV,
+	RelativeStrengthIndex: RSI,
+	RSI,
+	SimpleMovingAverage: SMA,
+	SMA,
+	StochasticOscillator: SO,
+	SO,
 }
