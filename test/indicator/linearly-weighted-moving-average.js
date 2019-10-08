591b267873ed5a5d728163b22931691e1e104e3f
codeplayr <nufan2k@gmx.de>
2019-10-09 00:01:47 +0200

    add testcase

--- test/indicator/linearly-weighted-moving-average.js
+++ test/indicator/linearly-weighted-moving-average.js
@@ -37,5 +37,15 @@ describe('Lineary wighted moving average', () =>{
         })();
     });
 
+    it('should calculate correctly with custom options and return results', () => {
+        (async () => {
+            let clonedData = [ ...data ];
+            clonedData.unshift(5);
+            clonedData.push(20);
+            let opts = {periods: 4, startIndex: 1, endIndex: clonedData.length - 2, lazyEvaluation: true};
+            let r = await runTest( opts, clonedData )
+            assertResults( r, expectedResults);
+        })();
+    });
 
 });
