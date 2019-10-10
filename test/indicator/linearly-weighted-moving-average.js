3cb5f6eebb616fb0bb49b50c9b187702ff17ad67
codeplayr <nufan2k@gmx.de>
2019-10-10 21:45:09 +0200

    add testcase

--- test/indicator/linearly-weighted-moving-average.js
+++ test/indicator/linearly-weighted-moving-average.js
@@ -29,6 +29,18 @@ describe('Lineary wighted moving average', () =>{
         });
     };
 
+    let assertResults_2 = ( results, expectedResults ) => {
+        assert.isArray( results );
+        results.forEach( ( result, idx ) => { 
+            assert.isObject( result );
+            assert.containsAllKeys( result, ['price', 'lwma'] );
+            assert.isNumber( result.price ); 
+            assert.isNumber( result.lwma ); 
+            assert.closeTo( result.lwma, expectedResults[idx].lwma , 0.1 );
+            assert.isTrue( result.price == expectedResults[idx].price );
+        });
+    };       
+
     it('should calculate correctly and return results', () => {
         (async () => {
             let opts = {periods: periods, lazyEvaluation: true};
@@ -45,6 +57,27 @@ describe('Lineary wighted moving average', () =>{
             let opts = {periods: 4, startIndex: 1, endIndex: clonedData.length - 2, lazyEvaluation: true};
             let r = await runTest( opts, clonedData )
             assertResults( r, expectedResults);
+        })();
+    });
+
+    it('should calculate correctly and return results with included offset', () => {
+        (async () => {
+            let clonedExpectedResults = [ ...expectedResults ];
+            let opts = {periods: periods, sliceOffset: false, lazyEvaluation: true};
+
+            for(let i=0; i<opts.periods - 1; i++){
+                clonedExpectedResults.unshift( 0 );
+            }
+
+            let expected = clonedExpectedResults.map( (val, idx ) => {
+                return {
+                    lwma: val,
+                    price: data[ idx ],
+                };
+            });
+
+            let r = await runTest( opts, data );
+            assertResults_2( r, expected);
         })();
     });
 
