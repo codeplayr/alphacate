cc97a2e35cf6bfb430c24ef3163920e0be1bb964
codeplayr <nufan2k@gmx.de>
2019-10-14 21:52:19 +0200

    add testcase

--- test/utils/misc.js
+++ test/utils/misc.js
@@ -13,5 +13,17 @@ describe( 'Misc Utils', () =>{
         assert.isFalse( Misc.has( 123, 'bar' ) );
         assert.isFalse( Misc.has( '123', 'bar' ) );
     });
+    
+    it('should extends objects', () => {
+        let obj_1 = {a: 'a', b: 'b'};
+        let obj_2 = {b: 2, c: 'c'};
+        let result = Misc.extends( obj_1, obj_2 );
+
+        assert.isObject( result );
+        assert.containsAllKeys( result, ['a', 'b', 'c'] );
+        assert.isTrue( result.a === obj_1.a );
+        assert.isTrue( result.b === obj_2.b );
+        assert.isTrue( result.c === obj_1.c );
+    });
 
 });
