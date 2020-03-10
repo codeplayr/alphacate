328510f79b1937b2593a5bbdf185c1df68767eef
codeplayr <nufan2k@gmx.de>
2020-03-10 01:22:14 +0100

    add test description

--- test/indicator/weighted-moving-average.js
+++ test/indicator/weighted-moving-average.js
@@ -3,3 +3,12 @@
 const assert = require('chai').assert;
 const _ = require('underscore');
 const WMA = require('./../../lib/indicator/weighted-moving-average');
+
+describe('WMA', () => {
+
+
+    it('should calculate correctly and return results', () => {
+
+    });
+        
+});   
