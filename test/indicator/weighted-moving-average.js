a6da50f6b75226dca9a4b1c1a9811be8fd822e2b
codeplayr <nufan2k@gmx.de>
2020-03-11 23:45:20 +0100

    add test case

--- test/indicator/weighted-moving-average.js
+++ test/indicator/weighted-moving-average.js
@@ -6,9 +6,24 @@ const WMA = require('./../../lib/indicator/weighted-moving-average');
 
 describe('WMA', () => {
 
+    let data = [90.91, 90.83, 90.28, 90.36, 90.90, 90.51];
+
+    let runTest = async ( data, expectedResults, options = {} ) => {
+        let wma = new WMA( options ); 
+        wma.setValues( data );
+        let results = await wma.calculate();
+        results.forEach( (item, idx) => {
+            assert.containsAllKeys( item, ['price', 'wma'] );
+            assert.closeTo( item.wma, expectedResults[ idx ], 0.02 );
+            assert.isTrue( item.price > 0 ); 
+        });
+    };
 
     it('should calculate correctly and return results', () => {
-
+        ( async () => {
+            let expectedResults = [90.62, 90.57];
+            await runTest( data, expectedResults, {periods: 5, sliceOffset: true} );
+      })();
     });
         
 });   
