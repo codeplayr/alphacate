e0758a0de02db3a26013ea0f5fe6b676927233dd
codeplayr <nufan2k@gmx.de>
2020-03-20 12:15:44 +0100

    separate test method

--- test/indicator/weighted-moving-average.js
+++ test/indicator/weighted-moving-average.js
@@ -21,43 +21,45 @@ describe('WMA', () => {
         });
     };
 
-    it('should calculate correctly and return results', () => {
+    let runTest_2 = async (opts, data, expectedResult) => {
+        let sma = new WMA( opts );
+        sma.setValues( data );
+        let results = await sma.calculate();
+
+        assert.isArray( results )
+        assert.isTrue( expectedResult.length == results.length );
+        results.forEach( (item, idx) => {
+            assert.isObject( item );
+            assert.containsAllKeys( item, ['price', 'wma'] );
+            assert.isNumber( item.price );
+            assert.isNumber( item.wma );
+            assert.closeTo( expectedResult[idx].wma, item.wma, 0.1 );
+            assert.isTrue( item.price == data[ opts.startIndex + opts.periods - 1 + idx ] );
+        });
+    }
+
+    it('should calculate correctly and return results', ( done ) => {
         ( async () => {
             let expectedResults = [90.62, 90.57];
             await runTest( data, expectedResults, {periods: 5, sliceOffset: true} );
      
             let expectedResults2 = [0,0,0,0].concat(expectedResults);
             await runTest( data, expectedResults2, {periods: 5, sliceOffset: false} );
+
+            done();
         })();
     });
 
     it('should calculate correctly with starIndex and endIndex options and return result', ( done ) => {
-        let opts = { periods: 2, startIndex: 1, endIndex: data.length - 2, lazyEvaluation: true, sliceOffset: true };
-
-		let runTest = async (opts, data, expectedResult) => {
-			let sma = new WMA( opts );
-			sma.setValues( data );
-            let results = await sma.calculate();
-
-			assert.isArray( results )
-            assert.isTrue( expectedResult.length == results.length );
-            results.forEach( (item, idx) => {
-				assert.isObject( item );
-				assert.containsAllKeys( item, ['price', 'wma'] );
-				assert.isNumber( item.price );
-				assert.isNumber( item.wma );
-				assert.closeTo( expectedResult[idx].wma, item.wma, 0.1 );
-				assert.isTrue( item.price == data[ opts.startIndex + opts.periods -1 + idx ] );
-			});
-		}
-
 		(async function(){
+            let opts = { periods: 2, startIndex: 1, endIndex: data.length - 2, lazyEvaluation: true, sliceOffset: true };
+
 			let expectedResult =   [
                 { price: 90.28, wma: 90.46 },
                 { price: 90.36, wma: 90.33 },
                 { price: 90.9, wma: 90.72 } 
             ];
-            await runTest( opts, data, expectedResult );
+            await runTest_2( opts, data, expectedResult );
             done();
         })();
 
