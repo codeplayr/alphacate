3bde124e2774005d41586f6fbc7b029cf5d33e7b
codeplayr <nufan2k@gmx.de>
2020-04-20 23:34:23 +0200

    add file

---
+++ test/indicator/accumulation-distribution-line.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('chai').assert;
+const _ = require('underscore');
+const ADL = require('./../../lib/indicator/accumulation-distribution-line');
+
+describe('Accumulation Distribution Line', () => {
+
+});
