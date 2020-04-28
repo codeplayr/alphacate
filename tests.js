4a8be0fc4bb3c8de25fbcf0d1519074639ab49f5
codeplayr <nufan2k@gmx.de>
2020-04-28 23:41:42 +0200

    import test

--- tests.js
+++ tests.js
@@ -14,3 +14,4 @@ require('./test/indicator/stochastic-oscillator');
 require('./test/indicator/moving-average-convergence-divergence');
 require('./test/indicator/money-flow-index');
 require('./test/indicator/weighted-moving-average');
+require('./test/indicator/accumulation-distribution-line');
