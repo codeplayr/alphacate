287e87022548c3fdf9ee93e014f0c80ee70b23e3
codeplayr <nufan2k@gmx.de>
2020-05-11 23:50:10 +0200

    add test file

---
+++ test/indicator/triple-exponential-average.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const assert = require('chai').assert;
+const _ = require('underscore');
+const TRIX = require('./../../lib/indicator/triple-exponential-average');
+
+describe('TRIX', function(){
+
+    let data = [
+        102.32,
+        105.54,
+        106.59,
+        107.39,
+        107.45,
+        109.08,
+        109.07,
+        109.10,
+        106.09,
+        106.72,
+        107.90,
+        106.50,
+        108.88,
+        109.82,
+        110.97,
+        110.96,
+        110.24,
+        109.70,
+        109.68,
+        112.16,
+    ];
+
+});
